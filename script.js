20329b6d9338d0e53bb495bfb8ccd5c928db33f9
rraabe <49948103+rraabe@users.noreply.github.com>
2019-07-11 16:18:28 -0400

    testing if the same change causes a conflict

--- script.js
+++ script.js
@@ -5,78 +5,67 @@ L.tileLayer("https://{s}.tile.OpenStreetMap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-
-
 var popup = L.popup();
 
 function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
+  popup
+    .setLatLng(e.latlng)
+    .setContent("You clicked the map at " + e.latlng.toString())
+    .openOn(map);
 }
 
-map.on('click', onMapClick);
-
-
-
+map.on("click", onMapClick);
 
 /////////
 
-
-
-fetch('all.json')
-  .then(function (response) {
+fetch("all.json")
+  .then(function(response) {
     return response.json();
   })
-  .then(function (allResteronts) {
+  .then(function(allResteronts) {
     allResteronts.forEach(getInfo);
-    allResteronts.forEach(makeMarker)
-  })
+    allResteronts.forEach(makeMarker);
+  });
 
-
-
-
-function getInfo (theResteront) {
-
-fetch(`${theResteront}.json`)
-.then(function (response) {
-    return response.json();
-  })
-  .then(function (resteront) {
-    addName(resteront)
-    setAddress(resteront.address)
-  })
-};
+function getInfo(theResteront) {
+  fetch(`${theResteront}.json`)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(resteront) {
+      addName(resteront);
+      setAddress(resteront.address);
+    });
+}
 
 function addName(theResteront) {
-    
+  const element = document.getElementById("container");
 
-const element = document.getElementById('container');
-  
-  const name = document.createElement('a');
+  const name = document.createElement("a");
   name.textContent = theResteront.name;
-  name.href=`http://localhost:8080/restaurant.html#${theResteront};`
-  
+  name.href = `http://localhost:8080/restaurant.html#${theResteront.id}`;
+
   element.appendChild(name);
 }
 
-
-function setAddress(address){
-    let urlAddress = encodeURIComponent(address);
-    console.log(`https://nominatim.openstreetmap.org/search/?q=${urlAddress}&format=json`);
-    fetch(`https://nominatim.openstreetmap.org/search/?q=${urlAddress}&format=json`)
-      .then(request => request.json())
-      .then(json => {
-        console.log(json[0].lat); //This just logs the lat. Change it to focus the map. 
-        console.log(json[0].lon);
-        L.marker([json[0].lat, json[0].lon]) ///make universal
-  .addTo(map)
-  .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-  .openPopup();
-      })
-    }
-
+function setAddress(address) {
+  let urlAddress = encodeURIComponent(address);
+  console.log(
+    `https://nominatim.openstreetmap.org/search/?q=${urlAddress}&format=json`
+  );
+  fetch(
+    `https://nominatim.openstreetmap.org/search/?q=${urlAddress}&format=json`
+  )
+    .then(request => request.json())
+    .then(json => {
+      console.log(json[0].lat); //This just logs the lat. Change it to focus the map.
+      console.log(json[0].lon);
+      L.marker([json[0].lat, json[0].lon]) ///make universal
+        .addTo(map)
+        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+        .openPopup();
+    });
+}
 
 // fetch(https://nominatim.openstreetmap.org/search/?q=182 Main St.,Burlington,VT&format=json)
 // .then(function (response) {
