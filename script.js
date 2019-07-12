ba2733c7390162b942f34988f55abf58008ffc06
rraabe <49948103+rraabe@users.noreply.github.com>
2019-07-12 17:09:48 -0400

    All JS is now in script.js and shared for both pages

--- script.js
+++ script.js
@@ -1,43 +1,49 @@
-var map = L.map("map").setView([44.477299, -73.213094], 16); 
-
-L.tileLayer("https://{s}.tile.OpenStreetMap.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-//////do not need vvvvvvvvvvvvvv
-var popup = L.popup();
-
-function onMapClick(e) {
-  popup
-    .setLatLng(e.latlng)
-    .setContent("You clicked the map at " + e.latlng.toString())
-    .openOn(map);
-}
-
-map.on("click", onMapClick);
-
-/////////^^^^^do not need^^^^^^
-
-fetch("all.json")
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(allRestaurants) {
-    allRestaurants.forEach(getInfo);
-    allRestaurants.forEach(makeMarker); //This is throwing an error
-  });
-
-function getInfo(theRestaurant) {
-  fetch(`${theRestaurant}.json`)
+//If you're on the homepage get all the restaurant names and iterate over fetchRestaurantInfoPromise
+if (window.location == "http://127.0.0.1:8080/") {
+  fetch("all.json")
     .then(function(response) {
       return response.json();
     })
-    .then(function(restaurant) {
-      addName(restaurant);
-      setAddress(restaurant.name, restaurant.address, restaurant.id);
+    .then(function(allRestaurants) {
+      allRestaurants.forEach(fetchRestaurantInfoPromise);
+    });
+}else{ //Otherwise only use fetchRestaurantInfoPromise for the restaurant determined by the hash
+  let fragment = window.location.hash.slice(1);
+  fetchRestaurantInfoPromise(fragment);
+}
+
+//Gets all the restaurant information
+function fetchRestaurantInfoPromise(restaurant) {
+  return fetch(`/${restaurant}.json`)
+    .then(request => request.json())
+    .then(json => appendLatLon(json));
+}
+
+//Adds the latitude and longitude to the restaurant object
+function appendLatLon(rest) {
+  let urlAddress = encodeURIComponent(rest.address);
+  fetch(
+    `https://nominatim.openstreetmap.org/search/?q=${urlAddress}&format=json`
+  )
+    .then(request => request.json())
+    .then(json => {
+      rest.lat = json[0].lat;
+      rest.lon = json[0].lon;
+      return rest;
+    })
+    .then(fullRestInfo => {
+      //If you are on the restaurant page use buildRestaurantPage to add all the relevant info to the page and add the map with makeMap
+      if (window.location != "http://127.0.0.1:8080/") {
+        buildRestaurantPage(fullRestInfo);
+        makeMap(fullRestInfo);
+      } else {//Otherwise you're on the homepage and you just need to make the map and add the href locations to the names. 
+        makeMap(fullRestInfo);
+        addName(fullRestInfo);
+      }
     });
 }
 
+//Adds the restaurant names with links to the homepage.
 function addName(theRestaurant) {
   const element = document.getElementById("container");
 
@@ -48,39 +54,63 @@ function addName(theRestaurant) {
   element.appendChild(name);
 }
 
-function setAddress(name, address, id) {
-  let urlAddress = encodeURIComponent(address);
-  console.log(
-    `https://nominatim.openstreetmap.org/search/?q=${urlAddress}&format=json`
-  );
-  fetch(
-    `https://nominatim.openstreetmap.org/search/?q=${urlAddress}&format=json`
-  )
-    .then(request => request.json())
-    .then(json => {
-      console.log(json[0].lat); //This just logs the lat. Change it to focus the map.
-      console.log(json[0].lon);
-      L.marker([json[0].lat, json[0].lon]) ///make universal
-        .addTo(map)
-        .bindPopup(`${name}<br>${address}`)
-        .on('mouseover', function (e) {
-          this.openPopup();
-         })
-        .on('mouseout', function (e) {
-          this.closePopup();
-         })
-        .on('click', function(e){
-          window.location = `/restaurant.html#${id}`;
-        });
-    });
+//Creat
+function buildRestaurantPage(restObject) {
+  let contentContainer = document.getElementById("content-container");
+
+  let name = (contentContainer.appendChild(
+    document.createElement("h1")
+  ).textContent = restObject.name);
+  let address = (contentContainer.appendChild(
+    document.createElement("p")
+  ).textContent = restObject.address);
+  let phone = (contentContainer.appendChild(
+    document.createElement("p")
+  ).textContent = restObject.phone);
+  let hours = (contentContainer.appendChild(
+    document.createElement("p")
+  ).textContent = restObject.hours);
+  let website = contentContainer.appendChild(document.createElement("a"));
+  website.textContent = restObject.website;
+  website.href = restObject.website;
+
+  restObject.notes.forEach(note => {
+    document
+      .getElementById("notes-container")
+      .appendChild(document.createElement("p")).innerHTML = marked(note);
+  });
 }
 
+let map = false;
 
+function makeMap(restObject) {
+  if (window.location == "http://127.0.0.1:8080/" && map == false) {
+    map = L.map("map").setView([44.477299, -73.213094], 16);
+  } else if (map == false) {
+    map = L.map("map").setView([restObject.lat, restObject.lon], 20);
+  }
 
-// fetch(https://nominatim.openstreetmap.org/search/?q=182 Main St.,Burlington,VT&format=json)
-// .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (allRestaurants) {
-//     allRestaurants.forEach(addPost);
-//   })
+  L.tileLayer("https://{s}.tile.OpenStreetMap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  L.marker([restObject.lat, restObject.lon])
+    .addTo(map)
+    .bindPopup(`${restObject.name}<br>${restObject.address}`)
+    .on("mouseover", function(e) {
+      this.openPopup();
+    })
+    .on("mouseout", function(e) {
+      this.closePopup();
+    })
+    .on("click", function(e) {
+      if (window.location == "http://127.0.0.1:8080/") {
+        window.location = `http://127.0.0.1:8080/restaurant.html#${
+          restObject.id
+        }`;
+      } else {
+        window.location = `${restObject.website}`;
+      }
+    });
+}
